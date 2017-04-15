22f6ce72f451f1d0f26b7c2bbb9bfbb44ee5236c
Shane Gough <shane@thegaragelab.com>
2017-04-15 18:14:21 +0700

    Added tests for Debug extension methods.

---
+++ TheGarageLab.Logging.Test/TestData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TheGarageLab.Logging.Test
+{
+    /// <summary>
+    /// Set up the test data in a single location
+    /// </summary>
+    public class TestData
+    {
+        /// <summary>
+        /// Data to use for testing non-null messages
+        /// </summary>
+        public readonly string TestMessage = "My Message";
+
+        /// <summary>
+        /// Data to use for testing non-null exceptions
+        /// </summary>
+        public readonly Exception TestException = new Exception();
+
+        /// <summary>
+        /// Data to use for testing non-null format strings
+        /// </summary>
+        public readonly string TestFormat = "0 = {0}, 1 = {1}";
+
+        /// <summary>
+        /// Data for first format string argument
+        /// </summary>
+        public readonly string TestFormatArg0 = "arg0";
+
+        /// <summary>
+        /// Data for second format string argument
+        /// </summary>
+        public readonly string TestFormatArg1 = "arg1";
+
+        /// <summary>
+        /// The resulting formatted string
+        /// </summary>
+        public readonly string TestFormatResult;
+
+        /// <summary>
+        /// Constructor to set up the test data
+        /// </summary>
+        protected TestData()
+        {
+            TestFormatResult = string.Format(TestFormat, TestFormatArg0, TestFormatArg1);
+        }
+    }
+}
---
+++ TheGarageLab.Logging.Test/TestDebugExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Xunit;
+using Moq;
+
+namespace TheGarageLab.Logging.Test
+{
+    /// <summary>
+    /// Test extension methods emitting Debug level logs
+    /// </summary>
+    public class TestDebugExtensions : TestData
+    {
+        /// <summary>
+        /// Constructor to set up test data
+        /// </summary>
+        public TestDebugExtensions() : base() { }
+
+        [Fact]
+        public void NullMessageWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<ArgumentNullException>(() => { mock.Object.Debug((string)null); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NonNullMessageWillCallProvider()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Object.Debug(TestMessage);
+            mock.Verify(logger => logger.Write(Severity.Debug, TestMessage, null), Times.Once());
+        }
+
+        [Fact]
+        public void NullFormatWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<ArgumentNullException>(() => { mock.Object.Debug((string)null, TestFormatArg0, TestFormatArg1); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NonNullFormatWillCallProvider()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Object.Debug(TestFormat, TestFormatArg0, TestFormatArg1);
+            mock.Verify(logger => logger.Write(Severity.Debug, TestFormatResult, null), Times.Once());
+        }
+
+        [Fact]
+        public void NonNullFormatWithIncorrectArgumentsWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<FormatException>(() => { mock.Object.Debug(TestFormat, TestFormatArg0); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NullCauseWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<ArgumentNullException>(() => { mock.Object.Debug((Exception)null); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NonNullCauseWillCallProvider()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Object.Debug(TestException);
+            mock.Verify(logger => logger.Write(Severity.Debug, TestException.GetType().Name, TestException), Times.Once());
+        }
+
+        [Fact]
+        public void NonNullCauseWithNullMessageWithFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<ArgumentNullException>(() => { mock.Object.Debug(TestException, null); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NonNullCauseWithNonNullMessageWillCallProvider()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Object.Debug(TestException, TestMessage);
+            mock.Verify(logger => logger.Write(Severity.Debug, TestMessage, TestException), Times.Once());
+        }
+
+        [Fact]
+        public void NonNullCauseWithNullFormatWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<ArgumentNullException>(() => { mock.Object.Debug(TestException, null, TestFormatArg0, TestFormatArg1); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+
+        [Fact]
+        public void NonNullCauseWithNonNullFormatWillCallProvider()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Object.Debug(TestException, TestFormat, TestFormatArg0, TestFormatArg1);
+            mock.Verify(logger => logger.Write(Severity.Debug, TestFormatResult, TestException), Times.Once());
+        }
+
+        [Fact]
+        public void NonNullCauseWithNonNullFormatWithIncorrectArgumentsWillFail()
+        {
+            var mock = new Mock<ILogger>();
+            Assert.Throws<FormatException>(() => { mock.Object.Debug(TestException, TestFormat, TestFormatArg0); });
+            mock.Verify(logger => logger.Write(It.IsAny<Severity>(), It.IsAny<string>(), It.IsAny<Exception>()), Times.Never());
+        }
+    }
+}
