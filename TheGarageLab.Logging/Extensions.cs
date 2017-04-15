9f0a786c784cbc5bddba4971528d2b666da1ddc8
Shane Gough <shane@thegaragelab.com>
2017-04-15 18:13:37 +0700

    Adding basic logging interface and extension methods for Debug level.

---
+++ TheGarageLab.Logging/Extensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using TheGarageLab.Ensures;
+
+namespace TheGarageLab.Logging
+{
+    /// <summary>
+    /// Extension methods for working with ILogger
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Emit a log entry with Debug severity and a message
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="message"></param>
+        public static void Debug(this ILogger logger, string message)
+        {
+            Ensure.IsNotNull<ArgumentNullException>(message);
+            logger.Write(Severity.Debug, message, null);
+        }
+
+        /// <summary>
+        /// Emit a log entry with Debug severity and a formatted message
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        public static void Debug(this ILogger logger, string format, params object[] args)
+        {
+            Ensure.IsNotNull<ArgumentNullException>(format);
+            logger.Write(Severity.Debug, string.Format(format, args), null);
+        }
+
+        /// <summary>
+        /// Emit a log entry with Debug severity and a cause
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="cause"></param>
+        public static void Debug(this ILogger logger, Exception cause)
+        {
+            Ensure.IsNotNull<ArgumentNullException>(cause);
+            logger.Write(Severity.Debug, cause.GetType().Name, cause);
+        }
+
+        /// <summary>
+        /// Emit a log entry with Debug severity, a cause and a message
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="cause"></param>
+        /// <param name="message"></param>
+        public static void Debug(this ILogger logger, Exception cause, string message)
+        {
+            Ensure.IsNotNull<ArgumentNullException>(cause);
+            Ensure.IsNotNull<ArgumentNullException>(message);
+            logger.Write(Severity.Debug, message, cause);
+        }
+
+        /// <summary>
+        /// Emit a log entry with Debug severity, a cause and a formatted message
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="cause"></param>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        public static void Debug(this ILogger logger, Exception cause, string format, params object[] args)
+        {
+            Ensure.IsNotNull<ArgumentNullException>(cause);
+            Ensure.IsNotNull<ArgumentNullException>(format);
+            logger.Write(Severity.Debug, string.Format(format, args), cause);
+        }
+    }
+}
---
+++ TheGarageLab.Logging/ILogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TheGarageLab.Logging
+{
+    /// <summary>
+    /// The severity level for a single log entry
+    /// </summary>
+    public enum Severity
+    {
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Fatal
+    }
+
+    /// <summary>
+    /// The simplest abstraction of logging functionality
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Write a single log entry
+        /// </summary>
+        /// <param name="entry"></param>
+        void Write(Severity severity, string message, Exception cause = null);
+    }
+}
